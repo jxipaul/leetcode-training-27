0cfc4fa1ce575eb2681a23d0bec573a4bec3e566
jxipaul <joeydsouza51@gmail.com>
2024-08-30 16:56:54 +0530

    FooBarBaz

--- week-01/solution.cpp
+++ week-01/solution.cpp
@@ -1,11 +1,18 @@
 #include<iostream>
 int main() {
     int a, b;
+    std::cout<<"Enter 2 numbers: ",a,b;
 
     std::cin >> a >> b;
 
     for(int i = a; i <=b; i++){
+        if (i%3==0)
+        std::cout<<"Foo";
+        else if (i%3!=0 && i%2==0)
+        std::cout<<"Bar";
+        else
+        std::cout<<"Baz";
 
-        
+
     }
 }
